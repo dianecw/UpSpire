b35d44cc6e814d7310f752643b1f6e5b93192d4b
Shannon <shannon@Shannons-MacBook-Air.local>
2015-06-27 10:27:45 -0700

    Added routing

--- bobaroos.js
+++ bobaroos.js
@@ -1,28 +1,35 @@
-Feelings = new Mongo.Collection("feelings");
+// Feelings = new Mongo.Collection("feelings");
 
-if (Meteor.isClient) {
-    Template.body.helpers({
-      feelings: function () {
-        return Feelings.find({}, {sort: {createdAt: -1}});
-      }
-    });
+// if (Meteor.isClient) {
+//     // Template.body.helpers({
+//     //   feelings: function () {
+//     //     return Feelings.find({}, {sort: {createdAt: -1}});
+//     //   }
+//     // });
 
-    Template.body.events({
-        'submit .feeling-submission': function () {
-            // Executed when form is submitted
-            var input = event.target.feeling.value;
-            Feelings.insert({
-                feeling: input,
-                createdAt: new Date()
-            });
-            event.target.feeling.value("done");
-            return false; // prevent default form behavior
-        }
-    });
-}
+//     // Template.body.events({
+//     //     'submit .feeling-submission': function () {
+//     //         // Executed when form is submitted
+//     //         var input = event.target.feeling.value;
+//     //         Feelings.insert({
+//     //             feeling: input,
+//     //             createdAt: new Date()
+//     //         });
+//     //         event.target.feeling.value("done");
+//     //         return false; // prevent default form behavior
+//     //     }
+//     // });
+// }
 
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // code to run on server at startup
-    });
-}
+// if (Meteor.isServer) {
+//     Meteor.startup(function () {
+//         // code to run on server at startup
+//     });
+// }
+
+Router.route('/', function () {
+  this.render('Home');
+});
+
+Router.route('/create');
+Router.route('/stream');
